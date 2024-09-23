cc75deb9291777ccf2e224428822e31c603758f8
jithin907 <jithink974@gmail.com>
2024-09-23 11:23:35 +0530

    Switch from BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
@@ -12,7 +12,7 @@ import { Home, Product, Products, AboutPage, ContactPage, Cart, Login, Register,
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -28,5 +28,5 @@ root.render(
         <Route path="/product/*" element={<PageNotFound />} />
       </Routes>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
